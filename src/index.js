e1accc08d5051165fd10359ba9f0575c8e45a7e4
alexandergureev <g.alex00@bk.ru>
2019-09-01 22:03:41 +0300

    configured hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StoreProvider } from "easy-peasy";
+
 import "react-virtualized/styles.css";
 import "antd/dist/antd.css";
 import "./index.css";
 
-import App from "./App";
+import store from "./store";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const render = Component =>
+  // eslint-disable-next-line react/no-render-return-value
+  ReactDOM.render(
+    <StoreProvider store={store}>
+      <Component />
+    </StoreProvider>,
+    document.getElementById("root")
+  );
+
+render(App);
+
+if (process.env.NODE_ENV === "development") {
+  if (module.hot) {
+    module.hot.accept("./components/App.js", () => {
+      render(App);
+    });
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 import { createStore } from "easy-peasy";
+
 import model from "./model";
 import Api from "../services/api";
 import Cache from "../services/cache";
 
-export default initialModel =>
-  createStore(initialModel || model, {
-    injections: { Api, cache: new Cache() }
-  });
+const store = createStore(model, {
+  injections: { Api, cache: new Cache() },
+  disableImmer: true
+});
+
+if (process.env.NODE_ENV === "development") {
+  if (module.hot) {
+    module.hot.accept("./model", () => {
+      store.reconfigure(model);
+    });
+  }
+}
+export default store;
