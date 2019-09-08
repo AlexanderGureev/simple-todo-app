84ad1978315d415864d3e4ad44e509b5f20fb977
alexandergureev <g.alex00@bk.ru>
2019-09-08 17:11:25 +0300

    preparation for deployment

--- src/components/Home/SocialBlock.js
+++ src/components/Home/SocialBlock.js
@@ -5,6 +5,9 @@ import GoogleLogin from "./GoogleLogin";
 import FacebookLogin from "./FacebookLogin";
 import { Form } from "./styles";
 
+const ENV = process.env.NODE_ENV === "production" ? "PROD" : "DEV";
+console.log(ENV);
+
 const SocialBlock = props => {
   const vkOnFailed = error => message.error(error);
   const vkOnSuccess = response => console.log(response);
@@ -22,23 +25,23 @@ const SocialBlock = props => {
     <Form.SocialBlock>
       <Form.SocialBlock.IconBox>
         <FacebookLogin
-          requestCodeUrl={process.env.REACT_APP_FACEBOOK_REQUEST_URL}
-          redirectUri={process.env.REACT_APP_FACEBOOK_REDIRECT_URL}
-          clientId={process.env.REACT_APP_FACEBOOK_CLIENT_ID}
+          requestCodeUrl={process.env[`REACT_APP_FACEBOOK_REQUEST_URL_${ENV}`]}
+          redirectUri={process.env[`REACT_APP_FACEBOOK_REDIRECT_URL_${ENV}`]}
+          clientId={process.env[`REACT_APP_FACEBOOK_CLIENT_ID_${ENV}`]}
           onFailure={facebookOnFailed}
           onSuccess={facebookOnSuccess}
         />
         <GoogleLogin
-          requestCodeUrl={process.env.REACT_APP_GOOGLE_REQUEST_URL}
-          redirectUri={process.env.REACT_APP_GOOGLE_REDIRECT_URL}
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          requestCodeUrl={process.env[`REACT_APP_GOOGLE_REQUEST_URL_${ENV}`]}
+          redirectUri={process.env[`REACT_APP_GOOGLE_REDIRECT_URL_${ENV}`]}
+          clientId={process.env[`REACT_APP_GOOGLE_CLIENT_ID_${ENV}`]}
           onFailure={googleOnFailed}
           onSuccess={googleOnSuccess}
         />
         <VKLogin
-          requestCodeUrl={process.env.REACT_APP_VK_REQUEST_URL}
-          redirectUri={process.env.REACT_APP_VK_REDIRECT_URL}
-          clientId={process.env.REACT_APP_VK_CLIENT_ID}
+          requestCodeUrl={process.env[`REACT_APP_VK_REQUEST_URL_${ENV}`]}
+          redirectUri={process.env[`REACT_APP_VK_REDIRECT_URL_${ENV}`]}
+          clientId={process.env[`REACT_APP_VK_CLIENT_ID_${ENV}`]}
           onFailure={vkOnFailed}
           onSuccess={vkOnSuccess}
         />
